94b6ef3977387412e823c96532efc9c6fedf265c
CoolTrol <bohdan1234yunior@gmail.com>
2023-05-13 14:01:59 +0300

    Credentials entity and repository

---
+++ src/main/java/com/demo/warmedicapp/services/impl/CredentialsServiceImpl.java
@@ -0,0 +1,9 @@
+package com.demo.warmedicapp.services.impl;
+
+import com.demo.warmedicapp.services.CredentialsService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CredentialsServiceImpl implements CredentialsService {
+
+}
